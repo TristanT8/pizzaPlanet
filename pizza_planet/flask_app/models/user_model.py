f25da3f0884ed497539323a2d4ee2c7039384f57
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-02-27 14:34:44 -0500

    Pizza planet users model

---
+++ pizza_planet/flask_app/models/user_model.py
@@ -0,0 +1,10 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app import app
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+
