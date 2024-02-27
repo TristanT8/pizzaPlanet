9b8164a883d75cf695df5166192f429e061dbf4c
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-02-27 14:46:00 -0500

    Pizza planet users model

--- pizza_planet/flask_app/models/user_model.py
+++ pizza_planet/flask_app/models/user_model.py
@@ -8,3 +8,14 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 
+class User:
+    my_db = "Pizza_planet"
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
