822775359eafd80cdf4ef4b3b3b09374854e8664
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-02-28 15:19:22 -0500

    Pizza planet users model

---
+++ pizza_planet/flask_app/controllers/user_cont.py
@@ -0,0 +1,8 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models.user_model import User
+
+
+@app.route('/')
+def welcome():
+    return render_template('login_register.html')
