05b1885556aaa56f9c4ce7ba79f308834ea9de96
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-03-01 14:07:04 -0500

    Pizza planet User Controller

--- pizza_planet/flask_app/controllers/user_cont.py
+++ pizza_planet/flask_app/controllers/user_cont.py
@@ -7,3 +7,12 @@ from flask_app.models.user_model import User
 def welcome():
     return render_template('pizza.html')
 
+
+@app.route('/register/user', methods = ['POST'])
+def register_user():
+    if not User.validate_register(request.form):
+        return redirect('/')
+    user_id = User.create_user(request.form)
+    session['user_id'] = user_id
+    return redirect('/')
+
