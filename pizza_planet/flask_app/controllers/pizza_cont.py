5be171ad192f88b172359fe6d814ea1d6214cacd
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-03-02 12:51:29 -0500

    Pizza planet minor adjustments

---
+++ pizza_planet/flask_app/controllers/pizza_cont.py
@@ -0,0 +1,6 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models.pizza_model import Pizza
+from flask_app.models.user_model import User
+
+
--- pizza_planet/server.py
+++ pizza_planet/server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-
+from flask_app.controllers.user_cont import User
 
 
 if __name__=="__main__":
