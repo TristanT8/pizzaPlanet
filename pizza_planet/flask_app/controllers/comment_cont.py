c07d2e2d217bfe37e54fa71bb5ebefd68abb7254
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-05-01 17:31:27 -0400

    Minor touch ups

--- pizza_planet/flask_app/controllers/comment_cont.py
+++ pizza_planet/flask_app/controllers/comment_cont.py
@@ -22,3 +22,29 @@ def add_comment():
         return redirect(url_for('view_pizza', pizza_id=pizza_id))
     else:
         return redirect(url_for('index'))  # Redirect to home if not a POST request or invalid request
+
+
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Comment:
+    my_db = "pizza_planet"
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.comment_text = data['comment_text']
+        self.pizza_id = data['pizza_id']
+
+    @classmethod
+    def create_comment(cls, data):
+        query = "INSERT INTO comments (comment_text, pizza_id) VALUES (%(comment_text)s, %(pizza_id)s);"
+        return connectToMySQL(cls.my_db).query_db(query, data)
+
+    @classmethod
+    def get_comments_by_pizza_id(cls, pizza_id):
+        query = "SELECT * FROM comments WHERE pizza_id = %(pizza_id)s;"
+        data = {"pizza_id": pizza_id}
+        results = connectToMySQL(cls.my_db).query_db(query, data)
+        comments = []
+        for result in results:
+            comments.append(cls(result))
+        return comments
