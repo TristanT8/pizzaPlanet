25a31a7cd66cd986ac4a249fb2d9c77f1554b667
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-05-04 13:06:37 -0400

    Edit/Delete comments

--- pizza_planet/flask_app/controllers/comment_cont.py
+++ pizza_planet/flask_app/controllers/comment_cont.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash, session
+from flask import render_template, request, redirect, url_for, flash
 from flask_app import app
 from flask_app.models.pizza_model import Pizza
 from flask_app.models.comment_model import Comment
@@ -23,47 +23,4 @@ def add_comment():
     else:
         return redirect(url_for('index'))  # Redirect to home if not a POST request or invalid request
 
-@app.route('/edit_comment/<int:comment_id>', methods=['GET', 'POST'])
-def edit_comment(comment_id):
-    if request.method == 'POST':
-        comment_text = request.form['comment']
-        
-        if not comment_text:
-            flash("Comment text is required.", "comment_error")
-            return redirect(url_for('edit_comment', comment_id=comment_id))
 
-        comment = Comment.get_comment_by_id(comment_id)
-        
-        # Check if the current user is the creator of the comment
-        if comment.user_id == session.get('user_id'):
-            data = {
-                'comment_text': comment_text
-            }
-            Comment.update_comment(comment_id, data)
-            flash("Comment updated successfully.", "comment_success")
-        else:
-            flash("You are not authorized to edit this comment.", "comment_error")
-
-        return redirect(url_for('view_pizza', pizza_id=comment.pizza_id))
-    else:
-        comment = Comment.get_comment_by_id(comment_id)
-        
-        # Check if the current user is the creator of the comment
-        if comment.user_id == session.get('user_id'):
-            return render_template('edit_comment.html', comment=comment)
-        else:
-            flash("You are not authorized to edit this comment.", "comment_error")
-            return redirect(url_for('view_pizza', pizza_id=comment.pizza_id))
-
-@app.route('/delete_comment/<int:comment_id>', methods=['POST'])
-def delete_comment(comment_id):
-    comment = Comment.get_comment_by_id(comment_id)
-    
-    # Check if the current user is the creator of the comment
-    if comment.user_id == session.get('user_id'):
-        Comment.delete_comment(comment_id)
-        flash("Comment deleted successfully.", "comment_success")
-    else:
-        flash("You are not authorized to delete this comment.", "comment_error")
-
-    return redirect(url_for('view_pizza', pizza_id=comment.pizza_id))
