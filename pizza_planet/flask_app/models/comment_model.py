6fdb9d52fbde5ac902a2b0a99af69192764f6a6f
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-05-04 13:06:46 -0400

    Edit/Delete comments

--- pizza_planet/flask_app/models/comment_model.py
+++ pizza_planet/flask_app/models/comment_model.py
@@ -7,11 +7,10 @@ class Comment:
         self.id = data['id']
         self.comment_text = data['comment_text']
         self.pizza_id = data['pizza_id']
-        self.user_id = data.get('user_id')  # Include user_id if available
 
     @classmethod
     def create_comment(cls, data):
-        query = "INSERT INTO comments (comment_text, pizza_id, user_id) VALUES (%(comment_text)s, %(pizza_id)s, %(user_id)s);"
+        query = "INSERT INTO comments (comment_text, pizza_id) VALUES (%(comment_text)s, %(pizza_id)s);"
         return connectToMySQL(cls.my_db).query_db(query, data)
 
     @classmethod
@@ -24,18 +23,4 @@ class Comment:
             comments.append(cls(result))
         return comments
 
-    @classmethod
-    def update_comment(cls, comment_id, data):
-        query = "UPDATE comments SET comment_text = %(comment_text)s WHERE id = %(comment_id)s;"
-        data['comment_id'] = comment_id
-        return connectToMySQL(cls.my_db).query_db(query, data)
-
-    @classmethod
-    def delete_comment(cls, comment_id):
-        query = "DELETE FROM comments WHERE id = %(comment_id)s;"
-        data = {"comment_id": comment_id}
-        return connectToMySQL(cls.my_db).query_db(query, data)
-
-    # Add method to check if the current user is the creator of the comment
-    def is_creator(self, user_id):
-        return self.user_id == user_id if self.user_id else False
+from flask_app.config.mysqlconnection import connectToMySQL
