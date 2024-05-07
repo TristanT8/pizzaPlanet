8d4f715a04cf1f328deb43eec9055dbb4b268007
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2024-05-07 13:47:09 -0400

    Edit/Delete comments

--- pizza_planet/flask_app/controllers/comment_cont.py
+++ pizza_planet/flask_app/controllers/comment_cont.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import request, redirect, url_for, flash, session
 from flask_app import app
 from flask_app.models.pizza_model import Pizza
 from flask_app.models.comment_model import Comment
@@ -8,14 +8,16 @@ def add_comment():
     if request.method == 'POST':
         comment_text = request.form['comment']
         pizza_id = request.form['pizza_id']
-        
+        user_id = session.get('user_id')  # Get user_id from session
+
         if not comment_text:
             flash("Comment text is required.", "comment_error")
             return redirect(url_for('view_pizza', pizza_id=pizza_id))
 
         data = {
             'comment_text': comment_text,
-            'pizza_id': pizza_id
+            'pizza_id': pizza_id,
+            'user_id': user_id  # Include user_id in data
         }
         Comment.create_comment(data)
         flash("Comment added successfully.", "comment_success")
